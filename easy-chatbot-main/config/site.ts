cfbd99acb18569c6679e5650d1f78d4e292491fd
lordofhep <122782975+lordofhep@users.noreply.github.com>
2024-08-09 13:54:15 +0400

    initial commit

---
+++ easy-chatbot-main/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Lucia",
+  description:
+    "AI-based interview site.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://x.com/_Harshit__S",
+    github: "https://github.com/",
+  },
+}
